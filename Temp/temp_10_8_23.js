4ad6f89dc0f97b62a43bfd443bfa5edbdc8973c4
aky360 <95171943+aky360@users.noreply.github.com>
2023-08-10 18:45:45 +0530

    Create temp_10_8_23.js

---
+++ Temp/temp_10_8_23.js
@@ -0,0 +1,53 @@
+import React, {useState} from "react";
+
+const App1 = () =>{
+
+    const [fullName, setFullName] = useState({
+        fname : "",
+        lname : ""
+    });
+
+    const inputEvent = (event) => {
+        console.log(event.target.value);
+        console.log(event.target.name);
+
+        const value = event.target.name;
+        const name = event.target.name;
+
+        setFullName((prev) => {
+            console.log(prev);
+        });
+    }
+
+    const onSubmits = (event) => {
+        event.preventDefault();
+    }
+
+    return (
+        <>
+            <div className="main_div">
+                <form onSubmit={onSubmits}>
+                <h1>hello {fullName.fname} {fullName.lname}</h1>
+                <input type="text" 
+                    placeholder="Enter your Name" 
+                    onChange={inputEvent} 
+                    name='fname'
+                    //value={fullName.fname} 
+                    />
+                <br />
+                <br />
+                <input type="text" 
+                    placeholder="Enter your Last Name" 
+                    onChange={inputEvent} 
+                    name='lname'
+                    //value={fullName.lname} 
+                    />
+                <button type="submit">Click me </button>
+                </form>
+            </div>
+        </>
+    )
+}
+
+
+export default App1;
